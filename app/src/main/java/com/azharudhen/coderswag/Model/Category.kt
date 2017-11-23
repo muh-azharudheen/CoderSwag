1db8042abd84ccbdee5a5f1224732f0ae7794936
Mac <mac@Macs-MacBook-Pro.local>
2017-11-23 14:28:27 +0400

    lesson-02

--- app/src/main/java/com/azharudhen/coderswag/Model/Category.kt
+++ app/src/main/java/com/azharudhen/coderswag/Model/Category.kt
@@ -3,4 +3,9 @@ package com.azharudhen.coderswag.Model
 /**
  * Created by mac on 11/22/17.
  */
-class Category(val title: String, val image:String)
+class Category(val title: String, val image:String){
+
+    override fun toString(): String {
+        return title
+    }
+}
